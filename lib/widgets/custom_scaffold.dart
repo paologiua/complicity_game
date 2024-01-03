728bff2fee9c76dc801e262de8b3838edcf836bf
Paolo Giua <paolo.giuaa@gmail.com>
2024-01-03 18:12:05 +0100

    Disabled the back navigation button on Android

--- lib/widgets/custom_scaffold.dart
+++ lib/widgets/custom_scaffold.dart
@@ -20,59 +20,62 @@ class CustomScaffold extends StatefulWidget {
 class _CustomScaffoldState extends State<CustomScaffold> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ThemeConstants.blueSecondaryColor,
-      body: SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-            color: ThemeConstants.bluePrimaryColor,
-            border: Border.all(
-              color: ThemeConstants.blueSecondaryColor,
-              style: BorderStyle.solid,
-              width: ThemeConstants.defaultBorder,
-            ),
-          ),
-          padding: const EdgeInsets.symmetric(
-            horizontal: ThemeConstants.defaultPadding,
-          ),
-          child: Center(
-            child: Container(
-              constraints: const BoxConstraints(
-                maxWidth: ThemeConstants.defaultMaxWidth,
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        backgroundColor: ThemeConstants.blueSecondaryColor,
+        body: SafeArea(
+          child: Container(
+            decoration: BoxDecoration(
+              color: ThemeConstants.bluePrimaryColor,
+              border: Border.all(
+                color: ThemeConstants.blueSecondaryColor,
+                style: BorderStyle.solid,
+                width: ThemeConstants.defaultBorder,
               ),
-              child: Stack(
-                children: [
-                  ...widget.children,
-                  if (widget.floatingButtons != null)
-                    Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            gradient: LinearGradient(
-                              colors: [
-                                ThemeConstants.bluePrimaryColor,
-                                ThemeConstants.bluePrimaryColor
-                                    .withOpacity(0.73),
-                                ThemeConstants.bluePrimaryColor.withOpacity(0),
-                              ],
-                              begin: const FractionalOffset(0.0, 1.0),
-                              end: const FractionalOffset(0.0, 0.0),
-                              stops: const [0.0, 0.3, 1.0],
-                              tileMode: TileMode.clamp,
+            ),
+            padding: const EdgeInsets.symmetric(
+              horizontal: ThemeConstants.defaultPadding,
+            ),
+            child: Center(
+              child: Container(
+                constraints: const BoxConstraints(
+                  maxWidth: ThemeConstants.defaultMaxWidth,
+                ),
+                child: Stack(
+                  children: [
+                    ...widget.children,
+                    if (widget.floatingButtons != null)
+                      Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Container(
+                            decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  ThemeConstants.bluePrimaryColor,
+                                  ThemeConstants.bluePrimaryColor
+                                      .withOpacity(0.73),
+                                  ThemeConstants.bluePrimaryColor.withOpacity(0),
+                                ],
+                                begin: const FractionalOffset(0.0, 1.0),
+                                end: const FractionalOffset(0.0, 0.0),
+                                stops: const [0.0, 0.3, 1.0],
+                                tileMode: TileMode.clamp,
+                              ),
                             ),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: widget.floatingButtons!
-                                .map((CustomFloatingButton button) => Padding(
-                                      padding: const EdgeInsets.all(
-                                          ThemeConstants.defaultPadding),
-                                      child: button,
-                                    ))
-                                .toList(),
-                          ),
-                        )),
-                ],
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: widget.floatingButtons!
+                                  .map((CustomFloatingButton button) => Padding(
+                                        padding: const EdgeInsets.all(
+                                            ThemeConstants.defaultPadding),
+                                        child: button,
+                                      ))
+                                  .toList(),
+                            ),
+                          )),
+                  ],
+                ),
               ),
             ),
           ),
